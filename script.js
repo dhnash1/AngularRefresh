69055e261b00c4e020a3895036134f496d3218fa
Deforis Nash <defhnash@gmail.com>
2017-12-22 15:09:04 -0600

    Working list function

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ console.log("JS Loaded");
 
 var App = angular.module('myApp',[]);
 App.controller("working",["$scope", "$http", function($scope, $http){
-  console.log("angular");
+  console.log("angular loaded");
+    var randomList = [];
+  $scope.listAdd = function(){
+    var itemToAdd = $scope.nameInput;
+    $scope.nameInput = "";
+    randomList.push(itemToAdd);
+    console.log(randomList);
+    $scope.randomList = randomList;
+  };
+  $scope.listRemove = function(){
+    console.log(this.$index);
+    var del = this.$index;
+    randomList.splice(del, 1);
+
+  };
 }]);
