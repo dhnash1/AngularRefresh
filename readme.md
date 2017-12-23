9952ce2934082340d5f2ed45f83c4bbc8e90556d
Deforis Nash <defhnash@gmail.com>
2017-12-23 16:00:12 -0600

    Changed Strings to objects, added random picker

--- readme.md
+++ readme.md
@@ -4,3 +4,8 @@ This program will:
 -Allow the user to input multiple different objects into a list
 -allow the user to push a button to randomly pick one object from the list
 -allow the user to delete any object from said list
+
+Technologies used:
+  -Angular
+  -HTML
+  -CSS
--- script.js
+++ script.js
@@ -4,17 +4,31 @@ var App = angular.module('myApp',[]);
 App.controller("working",["$scope", "$http", function($scope, $http){
   console.log("angular loaded");
     var randomList = [];
+    var x = 0;
   $scope.listAdd = function(){
     var itemToAdd = $scope.nameInput;
     $scope.nameInput = "";
-    randomList.push(itemToAdd);
+    var newObject = {
+      name : itemToAdd,
+      number : x
+    };
+    randomList.push(newObject);
     console.log(randomList);
     $scope.randomList = randomList;
+    x = x + 1;
   };
   $scope.listRemove = function(){
     console.log(this.$index);
     var del = this.$index;
     randomList.splice(del, 1);
 
+  };
+
+  $scope.randomPick = function(){
+    var arrNumb = randomList.length;
+    var numb = Math.random();
+    var rand = Math.round(numb * (arrNumb - 1));
+    console.log(rand);
+    console.log(randomList[rand].name);
   };
 }]);
