bd31d29d82ca955b65cd4341f7e3fde46c223677
Deforis Nash <defhnash@gmail.com>
2017-12-30 15:59:18 -0600

    Building out Item creation route for backend

--- models/itemMod.js
+++ models/itemMod.js
@@ -1,1 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
+var itemSchema = new Schema({
+  alias: {type: String, required : true},
+  item : String,
+  number : Number
+});
+
+var Item = mongoose.model('items', itemSchema); // items is the collection name
+
+module.exports = Item;
